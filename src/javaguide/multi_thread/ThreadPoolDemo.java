c29db3d3904fa776097436e7d220c8bb689d0133
javaliu2 <2359035384@qq.com>
2025-07-15 21:09:16 +0800

    605学习

--- src/javaguide/multi_thread/ThreadPoolDemo.java
+++ src/javaguide/multi_thread/ThreadPoolDemo.java
@@ -56,15 +56,19 @@ public class ThreadPoolDemo {
         }
 
         public void run() {
+            System.out.println(Thread.currentThread().getName() + " start.");
             Runnable task;
             while (true) {
                 synchronized (taskQueue) {
                     while (taskQueue.isEmpty() && !isShutdown) {
                         try {
+                            System.out.println(Thread.currentThread().getName() + " wait.");
                             taskQueue.wait();
+                            System.out.println(Thread.currentThread().getName() + " wake up.");
                         } catch (InterruptedException ignored) {}
                     }
                     if (isShutdown && taskQueue.isEmpty()) {
+                        System.out.println(Thread.currentThread().getName() + " exit.");
                         break; // 没有任务且线程池关闭，退出
                     }
                     task = taskQueue.removeFirst();
@@ -77,6 +81,21 @@ public class ThreadPoolDemo {
             }
             System.out.println(Thread.currentThread().getName() + " exiting.");
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPoolDemo obj = new ThreadPoolDemo(3);
+        Thread.sleep(500);
+        Runnable task = () -> {
+            System.out.println("Hello ThreadPool");
+        };
+        obj.submit(task);
+        Thread.sleep(500);
+        Runnable task2 = () -> {
+            System.out.println("Hello ThreadPool2");
+        };
+        obj.submit(task2);
+        obj.shutdown();
     }
 }
 
